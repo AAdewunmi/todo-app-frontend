d9879c0bc75c9c25d183e986111423aa6b88caff
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-09-13 09:19:08 +0100

    Seventh Commit: Implement demo react hello world app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.querySelector("#root"));
+ReactDOM.render(<App/>, document.querySelector("#root"));
