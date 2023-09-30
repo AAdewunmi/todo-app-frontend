56231ed0fffca915e652e35c255ce44a66ab1e18
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-09-30 13:00:05 +0100

    Tenth Commit: Create skeleton Form.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+const appTitle = "To-Do App"
+
 const App = () => {
-    return <h1>Hello World</h1>
+    return (
+      <div className="ui container center aligned">
+        <h1>App Title</h1>
+      </div>
+    );
 };
 
 export default App;
