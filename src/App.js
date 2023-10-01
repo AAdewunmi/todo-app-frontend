fc32bfe9ed1ff775575476469545444853826593
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-01 12:01:06 +0100

    Sixteenth Commit: Refactor form structure into sections

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Form from "./components/Form";
+import Section from "./components/Sections";
 
 
 const appTitle = "To-Do App"
 
 const App = () => {
-    return <div className="ui container center aligned">
-        <h1>{appTitle}</h1>
-        <br></br>
-        <Form />
-      </div>;
+    return (
+      <div className="ui container center aligned">
+        <Section>
+          <h1>{appTitle}</h1>
+        </Section>
+        <Section>
+          <Form />
+        </Section>
+      </div>
+    );
 };
 
 export default App;
