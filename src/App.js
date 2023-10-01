be7226a0c6135a8144fa1cb2142e3efbcdf24849
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-01 11:31:46 +0100

    Thirteenth Commit: Implement centering of form elements

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const appTitle = "To-Do App"
 const App = () => {
     return <div className="ui container center aligned">
         <h1>{appTitle}</h1>
+        <br></br>
         <Form />
       </div>;
 };
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,13 +3,13 @@ import React from "react";
 const Form = () => {
     return (
       <form className="ui form">
-        <div className="ui grid">
+        <div className="ui grid center aligned">
           <div className="row">
             <div className="column five wide">
-              <input type="text" />
+              <input type="text" placeholder="Enter To Do"/>
             </div>
             <div className="column one wide">
-              <button type="submit" className="ui button circular icon"><i className="plus icon"></i></button>
+              <button type="submit" className="ui button circular icon green"><i className="white plus icon"></i></button>
             </div>
           </div>
         </div>
