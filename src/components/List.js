b677fd5cda8493c86e53fbd601723acaf3133b93
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-04 08:08:44 +0100

    Twenty-Second Commit: Refactor List.js to include Todo.js as a component

--- src/components/List.js
+++ src/components/List.js
@@ -1,22 +1,9 @@
 import React from "react";
+import Todo from "./Todo";
 
 const List = () => {
     return (
-      <div className="ui grid center aligned">
-        <div className="row">
-          <div className="column five wide"><h2>Test</h2></div>
-          <div className="column one wide">
-            <button className="ui button circular icon green">
-              <i className="white check icon"></i>
-            </button>
-          </div>
-          <div className="column one wide">
-            <button className="ui button circular icon red">
-              <i className="white remove icon"></i>
-            </button>
-          </div>
-        </div>
-      </div>
+      <Todo />
     );
 };
 
---
+++ src/components/Todo.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Todo = () => {
+    return (
+      <div className="ui grid center aligned">
+        <div className="row">
+          <div className="column five wide">
+            <h2>Test</h2>
+          </div>
+          <div className="column one wide">
+            <button className="ui button circular icon green">
+              <i className="white check icon"></i>
+            </button>
+          </div>
+          <div className="column one wide">
+            <button className="ui button circular icon red">
+              <i className="white remove icon"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Todo;
