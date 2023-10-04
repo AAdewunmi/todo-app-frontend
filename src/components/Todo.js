4e5e8fbe9663003b2734a1a345e6c549e68a6686
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-04 08:21:20 +0100

    Twenty-Third Component: Implement edit todo item UI

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,24 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Todo = () => {
+    const[isEditing, setIsEditing] = useState(false);
+    
+    const handleDivDoubleClick = () => {
+        setIsEditing(true);
+    };
+    
     return (
-      <div className="ui grid center aligned">
-        <div className="row">
-          <div className="column five wide">
-            <h2>Test</h2>
-          </div>
-          <div className="column one wide">
-            <button className="ui button circular icon green">
-              <i className="white check icon"></i>
-            </button>
-          </div>
-          <div className="column one wide">
-            <button className="ui button circular icon red">
-              <i className="white remove icon"></i>
-            </button>
-          </div>
-        </div>
-      </div>
+        isEditing ? <input /> : 
+            <div className="ui grid center aligned">
+                <div className="row" onDoubleClick={handleDivDoubleClick}>
+                <div className="column five wide">
+                    <h2>Test</h2>
+                </div>
+                <div className="column one wide">
+                    <button className="ui button circular icon green">
+                    <i className="white check icon"></i>
+                    </button>
+                </div>
+                <div className="column one wide">
+                    <button className="ui button circular icon red">
+                    <i className="white remove icon"></i>
+                    </button>
+                </div>
+                </div>
+            </div>
     );
 };
 
