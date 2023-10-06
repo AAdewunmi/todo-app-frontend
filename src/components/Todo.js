b4a592f5d59e5f18a4bedf02303a78549273fff5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-06 10:12:27 +0100

    Twenty-Nineth Commit: Refactor Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -24,36 +24,36 @@ const Todo = ({ title }) => {
         setTempValue(e.target.value);
     }
 
-    return isEditing ? (
+    return (
       <div className="row" onDoubleClick={handleDivDoubleClick}>
-        <div className="column seven wide">
-          <div className="ui input fluid">
-            <input 
-            onChange={handleInputOnChange}
-            onKeyDown={handleInputKeyDown}
-            autoFocus={true}
-            value = {tempValue}
-            />
+        {isEditing ? (
+          <div className="column seven wide">
+            <div className="ui input fluid">
+              <input
+                onChange={handleInputOnChange}
+                onKeyDown={handleInputKeyDown}
+                autoFocus={true}
+                value={tempValue}
+              />
+            </div>
           </div>
-        </div>
-      </div>
-    ) : (
-      <div className="row" onDoubleClick={handleDivDoubleClick}>
-        <div className="ui grid center aligned">
-          <div className="column five wide">
-            <h2>{value}</h2>
+        ) : (
+          <div className="ui grid center aligned">
+            <div className="column five wide">
+              <h2>{value}</h2>
+            </div>
+            <div className="column one wide">
+              <button className="ui button circular icon green">
+                <i className="white check icon"></i>
+              </button>
+            </div>
+            <div className="column one wide">
+              <button className="ui button circular icon red">
+                <i className="white remove icon"></i>
+              </button>
+            </div>
           </div>
-          <div className="column one wide">
-            <button className="ui button circular icon green">
-              <i className="white check icon"></i>
-            </button>
-          </div>
-          <div className="column one wide">
-            <button className="ui button circular icon red">
-              <i className="white remove icon"></i>
-            </button>
-          </div>
-        </div>
+        )}
       </div>
     );
 };
